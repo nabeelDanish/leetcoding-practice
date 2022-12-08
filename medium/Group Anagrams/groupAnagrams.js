cab58f7c24aa990823c9a8657850ac85370ad909
nabeelDanishC <nabeel.danish@cowlar.com>
2022-12-08 18:17:26 +0500

    Improved Group Anagram Solution

--- medium/Group Anagrams/groupAnagrams.js
+++ medium/Group Anagrams/groupAnagrams.js
@@ -1,71 +1,34 @@
-var isAnagram = function (s, t) {
-  const letterMap = {};
-  if (s.length !== t.length) {
-    return false;
-  }
-
-  for (let i = 0; i < s.length; i++) {
-    const letter = s[i];
-    if (letterMap[letter] != null) {
-      letterMap[letter] += 1;
-    } else {
-      letterMap[letter] = 1;
-    }
-  }
-
-  for (let i = 0; i < t.length; i++) {
-    const letter = t[i];
-    if (letterMap[letter] != null) {
-      letterMap[letter] -= 1;
-      if (letterMap[letter] < 0) {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
-
-  for (key in Object.keys(letterMap)) {
-    if (letterMap[key] > 0) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
 /**
  * @param {string[]} strs
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-  const words = strs.map((word) => {
-    return [word, true];
-  });
+  const result = {};
 
-  const groups = [];
-  for (let i = 0; i < words.length; i++) {
-    const word = words[i];
-    if (!word[1]) {
-      continue;
-    }
-    const group = [word[0]];
+  for (let j = 0; j < strs.length; ++j) {
+    const str = strs[j];
 
-    for (let j = i + 1; j < words.length; j++) {
-      const otherWord = words[j];
-      if (!otherWord[1]) {
-        continue;
-      }
-      if (isAnagram(word[0], otherWord[0])) {
-        group.push(otherWord[0]);
-        words[j][1] = false;
-      }
+    // Getting an empty count
+    const count = [];
+    for (let i = 0; i < 26; ++i) {
+      count.push(0);
     }
 
-    groups.push(group);
+    // Now we compute its character count
+    for (let i = 0; i < str.length; i++) {
+      const letter = str[i];
+      const value = letter.charCodeAt(0) - "a".charCodeAt(0);
+      count[value] += 1;
+    }
+
+    if (result[count] != null) {
+      result[count].push(str);
+    } else {
+      result[count] = [str];
+    }
   }
 
-  return groups;
+  return Object.values(result);
 };
 
 const testGroupAnagrams = function (strs, output) {
