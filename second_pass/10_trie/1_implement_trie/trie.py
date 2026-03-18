df5aac5622572dd5923d78f2de8cf78719e5aaae
nabeelDanish <nabeelben@gmail.com>
2026-03-18 20:22:00 +0400

    refactor Trie implementation to enhance insert and search methods with detailed comments for clarity

--- second_pass/10_trie/1_implement_trie/trie.py
+++ second_pass/10_trie/1_implement_trie/trie.py
@@ -12,38 +12,50 @@ class TrieNode:
 class Trie:
 
     def __init__(self):
+        # we will have a dictionary of the first letters of the words in the trie, and each letter will point to a node that represents it,
         self.starts = {}
 
     def insert(self, word: str) -> None:
         n = len(word)
-        previous_node = self.starts.get(word[0], None)
+
+        # we will first check if the first letter of the word is already in the trie,
+        # if it is not, we will create a new node for it and add it to the starts dictionary
+        previous_node: TrieNode = self.starts.get(word[0], None)
         if previous_node is None:
             previous_node = TrieNode(word[0])
             self.starts[word[0]] = previous_node
 
+        # we will iterate through the word and add each letter to the trie
+        # if the letter is not already in the trie, we will create a new node for it and add it to the children of the previous node,
         for i in range(1, n):
             if word[i] not in previous_node.children:
                 previous_node.children[word[i]] = TrieNode(word[i])
             previous_node = previous_node.children[word[i]]
 
+        # after we have added all the letters to the trie, we will mark the last node as a word node
         previous_node.is_word = True
 
     def search(self, word: str) -> bool:
         n = len(word)
-        previous_node = self.starts.get(word[0], None)
+        
+        # we will first check if the first letter of the word is in the trie, if it is not, we can return false immediately, 
+        previous_node: TrieNode = self.starts.get(word[0], None)
         if previous_node is None:
             return False
 
+        # if it is, we will continue iterating through the word and checking if each letter is in the trie,
         for i in range(1, n):
             if word[i] not in previous_node.children:
                 return False
             previous_node = previous_node.children[word[i]]
 
+        # after we have iterated through the word, we will check if the last node is a word node, 
+        # if it is, we have found the word in the trie, otherwise, we have only found a prefix of the word in the trie
         return (previous_node and previous_node.is_word)
 
     def startsWith(self, prefix: str) -> bool:
         n = len(prefix)
-        previous_node = self.starts.get(prefix[0], None)
+        previous_node: TrieNode = self.starts.get(prefix[0], None)
         if previous_node is None:
             return False
 
